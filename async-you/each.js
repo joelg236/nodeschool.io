bc6f44f966851ce84c703f0400be94a4d589b270
Joel Gallant <joelgallant236@gmail.com>
2014-12-31 21:55:36 -0700

    Completed each

---
+++ async-you/each.js
@@ -0,0 +1,18 @@
+var http = require('http')
+  , async = require('async');
+
+async.each([process.argv[2], process.argv[3]], function(item, done){
+  http.get(item, function(res){
+    res.on('data', function(chunk){
+    });
+
+    res.on('end', function(){
+      return done();
+    });
+  }).on('error', function(err) {
+    return done(err);
+  });
+},
+function(err){
+  if (err) console.log(err);
+});
